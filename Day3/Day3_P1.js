b8c7737d57e0e3dfd62bca6a781807a0dfb11a99
Adam Pierce <adam.pierc3@gmail.com>
2018-12-10 13:46:25 -0800

    added comments to day3 q1

--- Day3/Day3_P1.js
+++ Day3/Day3_P1.js
@@ -16,21 +16,14 @@ for (let i = 0; i < 1000; i++) {
 
 // testArray = [["#1", "3,2:5x4"]];
 
-for(let i = 0; i < splitArray.length; i++) {
+for(let i = 0; i < splitArray.length; i++) {        //loop through the array of elf's ideas for cut locations in order to map each cut on the frequency array
 
     const id = splitArray[i][0];
     const [locInfo, size] = splitArray[i][1].split(':');
     const [fromLeft, fromTop] = locInfo.split(',');
     const [width, height] = size.toLowerCase().split('x');
-    // console.log(id);
-    // console.log(locInfo);
-    // console.log(size);
-    // console.log(fromLeft);
-    // console.log(fromTop);
-    // console.log(width);
-    // console.log(height);
 
-    for(let j = 0; j < height; j++) {
+    for(let j = 0; j < height; j++) {               //draw each elf's cut out on the freq map
         for(let k = 0; k < width; k++) {
             freqMap[parseInt(fromTop)+j][parseInt(fromLeft)+k]++;
         }
@@ -39,8 +32,8 @@ for(let i = 0; i < splitArray.length; i++) {
 
 }
 
+//count frequency map for overlapping cuts into the fabric
 let answer = 0;
-
 freqMap.forEach(arr => arr.forEach(freq => {
     if (freq > 1) {
         answer++;
