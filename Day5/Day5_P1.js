aa7c6126fb9bd10280363711b0bce4eed789cf76
Adam Pierce <adam.pierc3@gmail.com>
2018-12-13 15:03:44 -0800

    Day 5 Complete

--- Day5/Day5_P1.js
+++ Day5/Day5_P1.js
@@ -6,20 +6,18 @@ input = fs.readFileSync("./Day5/input.txt", "utf8");
 //PROBLEM 1
 test = "dabAcCaCBAcCcaDA";
 
-let answer = test;
+let answer = input;
 
 
 
 for (let i = 0; i < answer.length; i++) {
     if (answer.charAt(i) != answer.charAt(i+1) && answer.charAt(i).toUpperCase() === answer.charAt(i+1).toUpperCase()) {
-        console.log(i);
+        // console.log(i);
         answer = answer.slice(0,i) + answer.slice(i+2,answer.length);
-        console.log(answer);
+        // console.log(answer);
         i = i - 2;
     }
 }
 
+console.log(answer.length);
 
-
-
-// console.log(input);
