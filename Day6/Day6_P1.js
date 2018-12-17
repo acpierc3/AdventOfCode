77f668a9ec247f4ca60cc05e7aaa1173d2bc2d04
Adam Pierce <adam.pierc3@gmail.com>
2018-12-17 12:04:52 -0800

    Day6 P1

--- Day6/Day6_P1.js
+++ Day6/Day6_P1.js
@@ -4,12 +4,27 @@ let input = "";
 input = fs.readFileSync("./Day6/input.txt", "utf8");
 test = fs.readFileSync("./Day6/test.txt", "utf8");
 
+// let inputArray = test.split("\r\n");
+let inputArray = input.split("\r\n");
+
+inputArray = inputArray
+                    .map((record) => record.replace(' ',''))
+                    .map((record) => record.split(','))
+
 //PROBLEM 1
 
+//initialize a 2D array to map out coordinates from input
+const coordinateMap = new Array(400);
+for (let i = 0; i < 400; i++) {
+    coordinateMap[i] = new Array(400).fill(0);
+}
+
+for (let i =0; i<inputArray.length; i++) {
+
+}
 
 
-let inputArray = test.split("\r\n");
-// let inputArray = input.split("\r\n");
+
 
 
 
