11edc3d121082f9e44d1825944e6f9857a8d21c0
Adam Pierce <adam.pierc3@gmail.com>
2018-12-17 11:40:33 -0800

    Day6 P1

---
+++ Day6/Day5_P1.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+let input = "";
+
+input = fs.readFileSync("./Day5/input.txt", "utf8");
+
+//PROBLEM 1
+test = "dabAcCaCBAcCcaDA";
+
+let answer = input;
+
+
+
+for (let i = 0; i < answer.length; i++) {
+    if (answer.charAt(i) != answer.charAt(i+1) && answer.charAt(i).toUpperCase() === answer.charAt(i+1).toUpperCase()) {
+        // console.log(i);
+        answer = answer.slice(0,i) + answer.slice(i+2,answer.length);
+        // console.log(answer);
+        i = i - 2;
+    }
+}
+
+console.log(answer.length);
+
