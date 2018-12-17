77f93ab5b1f532a87c9e4b4c0d25c258831a8df9
Adam Pierce <adam.pierc3@gmail.com>
2018-12-17 12:31:50 -0800

    Day6 P1

--- Day6/Day6_P1.js
+++ Day6/Day6_P1.js
@@ -1,11 +1,14 @@
+"use strict";
+
 const fs = require("fs");
 let input = "";
+let test = "";
 
 input = fs.readFileSync("./Day6/input.txt", "utf8");
 test = fs.readFileSync("./Day6/test.txt", "utf8");
 
-// let inputArray = test.split("\r\n");
-let inputArray = input.split("\r\n");
+let inputArray = test.split("\r\n");
+// let inputArray = input.split("\r\n");
 
 inputArray = inputArray
                     .map((record) => record.replace(' ',''))
@@ -14,13 +17,38 @@ inputArray = inputArray
 //PROBLEM 1
 
 //initialize a 2D array to map out coordinates from input
-const coordinateMap = new Array(400);
-for (let i = 0; i < 400; i++) {
-    coordinateMap[i] = new Array(400).fill(0);
+const coordinateMap = new Array(10);
+for (let i = 0; i < 10; i++) {
+    coordinateMap[i] = new Array(10).fill(0);
 }
 
-for (let i =0; i<inputArray.length; i++) {
+let closestCoord = 0;
+let closestCoordDistance = 0;
+let distanceToCoord = -1;
 
+
+for (let y = 0; y < coordinateMap.length; y++) {
+    for (let x = 0; x < coordinateMap[y].length; x++) {
+
+        closestCoord = 0;
+        closestCoordDistance = 100;
+
+        for (let i = 0; i < inputArray.length; i++) {         //loop through input to find closest point
+           
+            distanceToCoord = Math.abs((y-inputArray[i][0]) + (x-inputArray[i][1]));
+            console.log(distanceToCoord);
+            
+            if (distanceToCoord === 0) {
+                closestCoord = i+1;
+                break;
+            } else if (distanceToCoord < closestCoordDistance) {
+                closestCoord = i+1;
+                closestCoordDistance = distanceToCoord;
+            }
+        }
+
+        coordinateMap[y][x] = closestCoord;
+    }
 }
 
 
@@ -29,5 +57,5 @@ for (let i =0; i<inputArray.length; i++) {
 
 
 
-console.log(inputArray);
+console.log(coordinateMap);
 
