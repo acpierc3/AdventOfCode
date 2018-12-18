c188311a40b8f62c80d02e196125da374cc34fc2
Adam Pierce <adam.pierc3@gmail.com>
2018-12-18 11:23:07 -0800

    Day6 P2

--- Day6/Day6_P2.js
+++ Day6/Day6_P2.js
@@ -7,8 +7,8 @@ let test = "";
 input = fs.readFileSync("./Day6/input.txt", "utf8");
 test = fs.readFileSync("./Day6/test.txt", "utf8");
 
-let inputArray = test.split("\r\n");
-// let inputArray = input.split("\r\n");
+// let inputArray = test.split("\r\n");
+let inputArray = input.split("\r\n");
 
 inputArray = inputArray
                     .map((record) => record.replace(' ',''))
@@ -17,34 +17,34 @@ inputArray = inputArray
 //PROBLEM 2
 
 //initialize a 2D array to map out coordinates from input
-const coordinateMap = new Array(10);
-for (let i = 0; i < 10; i++) {
-    coordinateMap[i] = new Array(10).fill(0);
+const coordinateMap = new Array(400);
+for (let i = 0; i < 400; i++) {
+    coordinateMap[i] = new Array(400).fill(0);
 }
 
 
+let distanceToCoord = 0;
+let totalDistance = 0;
+let answer = 0;
 
 
 for (let y = 0; y < coordinateMap.length; y++) {
     for (let x = 0; x < coordinateMap[y].length; x++) {
 
-        closestCoord = 0;
-        closestCoordDistance = 10000;
-        closestCoordCount = 0;
+        totalDistance = 0;
 
         for (let i = 0; i < inputArray.length; i++) {         //loop through input to find closest point
            
             distanceToCoord = Math.abs((x-inputArray[i][0])) + Math.abs((y-inputArray[i][1]));
-            // console.log(distanceToCoord);
-            
+            totalDistance = totalDistance + distanceToCoord;
+            // console.log(totalDistance);
             
         }
 
-        if (closestCoordCount > 1) {
-            coordinateMap[y][x] = ".";
-        } else {
-            coordinateMap[y][x] = closestCoord;
-            areaAroundCoord[closestCoord-1]++;
+        coordinateMap[y][x] = totalDistance;
+
+        if (totalDistance < 10000) {
+            answer++;
         }
         
     }
@@ -61,5 +61,4 @@ for (let y = 0; y < coordinateMap.length; y++) {
 
 
 // console.log(coordinateMap);
-// console.log(areaAroundCoord);
-console.log("Answer:", Math.max(...areaAroundCoord));
+console.log("Answer:",answer);
