21a272b0c42a63a3209314248dc531d3dfa243db
Adam Pierce <adam.pierc3@gmail.com>
2018-12-20 06:32:45 -0800

    Day7 P1

--- Day7/Day7_P1.js
+++ Day7/Day7_P1.js
@@ -4,15 +4,31 @@ const fs = require("fs");
 let input = "";
 let test = "";
 
-input = fs.readFileSync("./Day6/input.txt", "utf8");
-test = fs.readFileSync("./Day6/test.txt", "utf8");
+input = fs.readFileSync("./Day7/input.txt", "utf8");
+test = fs.readFileSync("./Day7/test.txt", "utf8");
 
-// let inputArray = test.split("\r\n");
-let inputArray = input.split("\r\n");
+let inputArray = test.split("\r\n");
+// let inputArray = input.split("\r\n");
 
 inputArray = inputArray
-                    .map((record) => record.replace(' ',''))
-                    .map((record) => record.split(','))
+                    .map((record) => record.match(/\b[A-Z]\b/g))
 
 //PROBLEM 1
 
+
+
+
+//array of remaining letters
+// let remainingLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+let remainingLetters = 'ABCDE'.split('');
+
+
+
+//buffer array of characters that are ready
+
+//final string of letters that are done
+
+
+
+
+console.log(inputArray);
