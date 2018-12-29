2ced248e31292ddeb2def2e7a8b31edf2ea30d8e
Adam Pierce <adam.pierc3@gmail.com>
2018-12-29 10:25:41 -0800

    Day8 P1

--- Day8/Day8_P1.js
+++ Day8/Day8_P1.js
@@ -7,33 +7,30 @@ let test = "";
 input = fs.readFileSync("./Day8/input.txt", "utf8");
 test = fs.readFileSync("./Day8/test.txt", "utf8");
 
-let inputArray = test.split(" ").map(x => +x);      //.map(x => +x) converts strings to numbers
-// let inputArray = input.split(" ");
+// let inputArray = test.split(" ").map(x => +x);      //.map(x => +x) converts strings to numbers
+let inputArray = input.split(" ").map(x => +x);
 
 
 
 //PROBLEM 1
 
-function nodeParse() {        //
+function nodeParse() {        //recursive function iterates through input array to return meta data
 
     const count = inputArray.shift();
     const meta = inputArray.shift();
 
     let answer = 0;
 
-    for (let x = 0; x < count; x++) {
+    for (let x = 0; x < count; x++) {   //iterate through number of child nodes
         answer += nodeParse();
     }
         
-
-    for (let x = 0; x < meta; x ++) {
+    for (let x = 0; x < meta; x ++) {   //iterate through number of meta data
         answer += inputArray.shift();
     }
 
-    console.log(inputArray);
+    // console.log(inputArray);
     return answer;
-    
-
 }
 
 
