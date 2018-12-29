4b9a93f57dfe2e6ed46f008d5c2b5a2c512dc85c
Adam Pierce <adam.pierc3@gmail.com>
2018-12-29 10:23:26 -0800

    Day8 P1

--- Day8/Day8_P1.js
+++ Day8/Day8_P1.js
@@ -7,12 +7,37 @@ let test = "";
 input = fs.readFileSync("./Day8/input.txt", "utf8");
 test = fs.readFileSync("./Day8/test.txt", "utf8");
 
-let inputArray = test.split(" ");
+let inputArray = test.split(" ").map(x => +x);      //.map(x => +x) converts strings to numbers
 // let inputArray = input.split(" ");
 
-inputArray = inputArray
-                    .map((record) => record.match(/\b[A-Z]\b/g))
+
 
 //PROBLEM 1
 
-console.log(inputArray)
+function nodeParse() {        //
+
+    const count = inputArray.shift();
+    const meta = inputArray.shift();
+
+    let answer = 0;
+
+    for (let x = 0; x < count; x++) {
+        answer += nodeParse();
+    }
+        
+
+    for (let x = 0; x < meta; x ++) {
+        answer += inputArray.shift();
+    }
+
+    console.log(inputArray);
+    return answer;
+    
+
+}
+
+
+console.log(nodeParse());
+
+
+
