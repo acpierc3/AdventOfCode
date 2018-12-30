81c8d24ac6499d45523e0e892b2d7c5712804e99
Adam Pierce <adam.pierc3@gmail.com>
2018-12-30 09:06:02 -0800

    Day8 P2

--- Day8/Day8_P2.js
+++ Day8/Day8_P2.js
@@ -7,8 +7,8 @@ let test = "";
 input = fs.readFileSync("./Day8/input.txt", "utf8");
 test = fs.readFileSync("./Day8/test.txt", "utf8");
 
-// let inputArray = test.split(" ").map(x => +x);      //.map(x => +x) converts strings to numbers
-let inputArray = input.split(" ").map(x => +x);
+let inputArray = test.split(" ").map(x => +x);      //.map(x => +x) converts strings to numbers
+// let inputArray = input.split(" ").map(x => +x);
 
 
 
@@ -21,16 +21,44 @@ function nodeParse() {        //recursive function iterates through input array 
 
     let answer = 0;
 
-    for (let x = 0; x < count; x++) {   //iterate through number of child nodes
-        answer += nodeParse();
-    }
+    if (count) {    //node has children
+
+        const childValues = [];
         
-    for (let x = 0; x < meta; x ++) {   //iterate through number of meta data
-        answer += inputArray.shift();
+
+        for (let x = 0; x < count; x++) {   //iterate through number of child nodes
+            childValues.push(nodeParse());
+        }
+
+        const metaValues = [];
+            
+        for (let x = 0; x < meta; x ++) {   //iterate through number of meta data
+            metaValues.push(inputArray.shift());
+        }
+
+        let answer = 0;
+
+        for (let x = 0; x < metaValues.length; x++) {
+            if (typeof childValues[metaValues[x]-1] != 'undefined') {   //child node specified by meta data exists
+                answer += childValues[metaValues[x]-1];
+            }
+        }
+
+        return answer;
+
+
+    } else {    //node has no children
+        let answer = 0;
+
+        for (let x = 0; x < meta; x ++) {   //iterate through number of meta data
+            answer += inputArray.shift();
+        }
+
+        console.log(inputArray);
+        return answer;
     }
 
-    // console.log(inputArray);
-    return answer;
+
 }
 
 
