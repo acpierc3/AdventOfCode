54b848a52db805d2c99ad6f6257cde1e8bc69c7d
Adam Pierce <adam.pierc3@gmail.com>
2018-12-31 09:44:20 -0800

    Day9 P1

--- Day9/Day9_P1.js
+++ Day9/Day9_P1.js
@@ -3,18 +3,20 @@
 //PROBLEM 1
 
 
-const numPlayers = 9;      //modify these two params based on puzzle input
-const lastMarble = 16;
+const numElfs = 9;      //modify these two params based on puzzle input
+const lastMarble = 25;
 
 const marbles = [0,1];
 let currentMarblePos = 1;
 
+const elfScores = new Array(numElfs);
 
 
 for(let x = 2; x <= lastMarble; x++) {
     
     if (x%23 === 0) {
-        
+        let elfWinner = x%numElfs;
+        console.log(elfWinner);
 
     } else if (currentMarblePos+1 === marbles.length) {
         marbles.splice(1,0,x);
