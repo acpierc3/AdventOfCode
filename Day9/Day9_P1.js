bd781854d811eedfa3e73a6f97ba55287fb1b336
Adam Pierce <adam.pierc3@gmail.com>
2018-12-31 10:17:08 -0800

    Day9 P1

--- Day9/Day9_P1.js
+++ Day9/Day9_P1.js
@@ -10,29 +10,28 @@ const marbles = [0,1];
 let currentMarblePos = 1;
 let newMarblePos = 0;
 
-const elfScores = new Array(numElfs);
+const elfScores = new Array(numElfs).fill(0);
 
 
 for(let x = 2; x <= lastMarble; x++) {
     
     if (x%23 === 0) {
         let elfWinner = x%numElfs;
-        console.log(elfWinner);
+        // console.log(elfWinner);
 
         if (currentMarblePos < 7) {
-            newMarblePos = marbles.length - (7 - currentMarblePos);
+            newMarblePos = marbles.length - 1 - (7 - currentMarblePos);
+            console.log("current:",currentMarblePos," new:", newMarblePos," array length:",marbles.length);
             
         } else {
             newMarblePos = currentMarblePos - 7;
         }
 
-        elfScores[elfWinner-1] += marbles[newMarblePos];
         currentMarblePos = newMarblePos;
         elfScores[elfWinner-1] += x + marbles[newMarblePos];
+        console.log("pos: ",newMarblePos, "curr marb: ", x, "removed marb: ", marbles[newMarblePos]);
         marbles.splice(newMarblePos, 1);
 
-        console.log();
-        
     } else if (currentMarblePos+1 === marbles.length) {
         marbles.splice(1,0,x);
         currentMarblePos = 1;
@@ -42,5 +41,9 @@ for(let x = 2; x <= lastMarble; x++) {
     }
 }
 
-console.log(marbles);
-console.log(elfScores);
+
+let highestPoints = Math.max(...elfScores);
+
+// console.log(marbles);
+// console.log(elfScores);
+console.log("winner:",highestPoints);
