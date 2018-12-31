d7b28fac529a3737b5e8bac1574b3eb57f37b607
Adam Pierce <adam.pierc3@gmail.com>
2018-12-31 10:01:15 -0800

    Day9 P1

--- Day9/Day9_P1.js
+++ Day9/Day9_P1.js
@@ -8,6 +8,7 @@ const lastMarble = 25;
 
 const marbles = [0,1];
 let currentMarblePos = 1;
+let newMarblePos = 0;
 
 const elfScores = new Array(numElfs);
 
@@ -18,6 +19,20 @@ for(let x = 2; x <= lastMarble; x++) {
         let elfWinner = x%numElfs;
         console.log(elfWinner);
 
+        if (currentMarblePos < 7) {
+            newMarblePos = marbles.length - (7 - currentMarblePos);
+            
+        } else {
+            newMarblePos = currentMarblePos - 7;
+        }
+
+        elfScores[elfWinner-1] += marbles[newMarblePos];
+        currentMarblePos = newMarblePos;
+        elfScores[elfWinner-1] += x + marbles[newMarblePos];
+        marbles.splice(newMarblePos, 1);
+
+        console.log();
+        
     } else if (currentMarblePos+1 === marbles.length) {
         marbles.splice(1,0,x);
         currentMarblePos = 1;
@@ -27,4 +42,5 @@ for(let x = 2; x <= lastMarble; x++) {
     }
 }
 
-console.log(marbles)
+console.log(marbles);
+console.log(elfScores);
