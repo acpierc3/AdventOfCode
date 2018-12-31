38d7cf0665b7e3940a02e59cccaf3774fc2462b3
Adam Pierce <adam.pierc3@gmail.com>
2018-12-31 10:39:44 -0800

    Day9 P1

--- Day9/Day9_P1.js
+++ Day9/Day9_P1.js
@@ -3,33 +3,42 @@
 //PROBLEM 1
 
 
-const numElfs = 9;      //modify these two params based on puzzle input
-const lastMarble = 25;
+const numElfs = 13;      //modify these two params based on puzzle input
+const lastMarble = 7999;
 
 const marbles = [0,1];
 let currentMarblePos = 1;
 let newMarblePos = 0;
+let currentPlayer = 2;
+let elfWinner = 0;
 
 const elfScores = new Array(numElfs).fill(0);
 
 
 for(let x = 2; x <= lastMarble; x++) {
     
+    if (currentPlayer === numElfs) {
+        currentPlayer = 1;
+    } else {
+        currentPlayer++;
+    }
+
+
     if (x%23 === 0) {
-        let elfWinner = x%numElfs;
-        // console.log(elfWinner);
+        // elfWinner = x%numElfs;
+        elfWinner = currentPlayer;
 
         if (currentMarblePos < 7) {
             newMarblePos = marbles.length - 1 - (7 - currentMarblePos);
-            console.log("current:",currentMarblePos," new:", newMarblePos," array length:",marbles.length);
+            // console.log("current:",currentMarblePos," new:", newMarblePos," array length:",marbles.length);
             
         } else {
             newMarblePos = currentMarblePos - 7;
         }
 
         currentMarblePos = newMarblePos;
-        elfScores[elfWinner-1] += x + marbles[newMarblePos];
-        console.log("pos: ",newMarblePos, "curr marb: ", x, "removed marb: ", marbles[newMarblePos]);
+        elfScores[elfWinner-1] += (x + marbles[newMarblePos]);
+        // console.log("pos: ",newMarblePos, "curr marb: ", x, "removed marb: ", marbles[newMarblePos]);
         marbles.splice(newMarblePos, 1);
 
     } else if (currentMarblePos+1 === marbles.length) {
