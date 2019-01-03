1963ff0310793e7e9dcc6997a0d13e3e5d8b9b0b
Adam Pierce <adam.pierc3@gmail.com>
2019-01-03 11:27:40 -0800

    Day10 P1

--- Day10/Day10_P1.js
+++ Day10/Day10_P1.js
@@ -5,8 +5,8 @@ let input = "";
 let test = "";
 
 
-input = fs.readFileSync("./Day8/input.txt", "utf8");
-test = fs.readFileSync("./Day8/test.txt", "utf8");
+input = fs.readFileSync("./Day10/input.txt", "utf8");
+test = fs.readFileSync("./Day10/test.txt", "utf8");
 
 // let inputArray = test.split(" ").map(x => +x);      //.map(x => +x) converts strings to numbers
 let inputArray = input.split(" ").map(x => +x);
@@ -15,27 +15,7 @@ let inputArray = input.split(" ").map(x => +x);
 
 //PROBLEM 1
 
-function nodeParse() {        //recursive function iterates through input array to return meta data
 
-    const count = inputArray.shift();
-    const meta = inputArray.shift();
-
-    let answer = 0;
-
-    for (let x = 0; x < count; x++) {   //iterate through number of child nodes
-        answer += nodeParse();
-    }
-        
-    for (let x = 0; x < meta; x ++) {   //iterate through number of meta data
-        answer += inputArray.shift();
-    }
-
-    // console.log(inputArray);
-    return answer;
-}
-
-
-console.log(nodeParse());
 
 
 
