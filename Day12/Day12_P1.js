462288363e5f1ce5c5ffb8fab611df69546ab3e2
Adam Pierce <adam.pierc3@gmail.com>
2019-01-05 11:00:15 -0800

    Day12 P1

--- Day12/Day12_P1.js
+++ Day12/Day12_P1.js
@@ -8,7 +8,8 @@ let test = "";
 input = fs.readFileSync("./Day12/input.txt", "utf8");
 test = fs.readFileSync("./Day12/test.txt", "utf8");
 
-let inputArray = input.split("\r\n");
+let inputArray = test.split("\r\n");
+// 
 let initialState = inputArray[0].replace("initial state: ","").trim();
 inputArray.shift();
 inputArray.shift();
@@ -20,9 +21,34 @@ for (let i = 0; i < inputArray.length; i++) {
 
 //PROBLEM 1
 
+const numGenerations = 2   //puzzle input
+
+let currentState = initialState;
+let plantZero = 0;
+
+for (let i = 1; i <= numGenerations; i++) {
+
+    if (currentState.charAt(0) === "#") {
+        currentState = ".." + currentState;
+        plantZero += 2;
+    } else if (currentState.charAt(1) === "#") {
+        currentState = "." + currentState;
+        plantZero += 1;
+    }
+
+    if (currentState.charAt(currentState.length-1) === "#") {
+        currentState = currentState + "..";
+    } else if (currentState.charAt(currentState.length-2) === "#") {
+        currentState = currentState + ".";
+    }
+
+    for(let j = 2; j < currentState.length-1; j++) {
+        console.log(currentState.charAt(j));
+    }
+}
 
 
 
-
-console.log(initialState);
-console.log(inputArray);
+// console.log(initialState);
+// console.log(inputArray);
+console.log(currentState);
